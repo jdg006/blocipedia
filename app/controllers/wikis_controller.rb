7202ec5de4e898c9d7a7edb9e44c4c9f6a0796ae
Joe <jdg006@lvc.edu>
2017-08-19 02:53:35 +0000

    checkpoint 4 complete

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,65 @@
+class WikisController < ApplicationController
+  def index
+     @wikis = Wiki.all
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+   
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  
+  def update
+     @wiki = Wiki.find(params[:id])
+     @wiki.assign_attributes(post_params)
+ 
+     if @wiki.save
+       flash[:notice] = "Post was updated."
+       redirect_to [@wiki]
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+  
+  def create
+  @wiki = Wiki.new
+     @wiki.title = params[:wiki][:title]
+     @wiki.body = params[:wiki][:body]
+     @wiki.private = false
+     @wiki.user = current_user
+
+     if @wiki.save
+       flash[:notice] = "Post was saved."
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
+   
+   def destroy
+     @wiki = Wiki.find(params[:id])
+ 
+     if @wiki.destroy
+       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
+   end
+   
+   private
+   
+   def post_params
+     params.require(:wiki).permit(:title, :body)
+   end
+end
